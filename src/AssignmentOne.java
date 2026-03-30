576c8cf0e7bb9d8e986f6931ce6a6fc61bb98f24
argbin <argbin05@gmail.com>
2026-03-30 17:45:49 +0200

    Add the function scenarioTwo()
    The purpose of the function is to have the user input any integer value and then print the factorial of that value. The function uses the absolute value
    of the input so that it does not matter if the user inputs a negative value.

--- src/AssignmentOne.java
+++ src/AssignmentOne.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 import java.util.Vector;
+import static java.lang.Math.abs;
 
 public class AssignmentOne{
     public static void main(String[] args){
-        scenarioOne();
+        scenarioTwo();
     }
 
     //A function that prompts the user for integers and then prints out the integers
@@ -25,6 +26,21 @@ public class AssignmentOne{
             System.out.println(number);
         }
 
+        sc.close();
+    }
+
+    //A function that prompts the user for an integer and then prints the factorial value of that integer.
+    public static void scenarioTwo(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a number!");
+        int input = abs(sc.nextInt());
+        int factorialNum = 1;
+
+        for(int i = input; i > 1; i--){
+            factorialNum *= i;
+        }
+
+        System.out.println("The factorial number would be: " + factorialNum);
         sc.close();
     }
 }
