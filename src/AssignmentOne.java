269dee5b486f23b4f2c84c9c048296e0318b25fc
argbin <argbin05@gmail.com>
2026-03-30 17:34:13 +0200

    Add the function scenarioOne().
    The purpuse of the function is to have the user input any number of integers and then print out those integers to the console.

--- src/AssignmentOne.java
+++ src/AssignmentOne.java
@@ -1,8 +1,28 @@
 import java.util.Scanner;
+import java.util.Vector;
 
 public class AssignmentOne{
     public static void main(String[] args){
-        System.out.println("hello world");
+        scenarioOne();
+    }
+
+    //A function that prompts the user for integers and then prints out the integers
+    public static void scenarioOne(){
         Scanner sc = new Scanner(System.in);
+        Vector<Integer> v = new Vector<Integer>();
+
+        System.out.println("How many integers would you like to input?");
+        int numberOfIntegers = sc.nextInt();
+
+        for(int i = 0; i < numberOfIntegers; i++){
+            System.out.println("Input number: " + (i+1));
+
+            v.add(sc.nextInt());
+        }
+
+        System.out.println("These where the inputed numbers:");
+        for(int number : v){
+            System.out.println(number);
+        }
     }
 }
